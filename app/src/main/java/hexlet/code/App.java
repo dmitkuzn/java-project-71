dac216c736929d6489299a1ceaee08319969c7dd
Dmitrii Kuznetsov <dmitkuzn@mail.ru>
2024-10-07 16:18:19 +0300

    picocli added

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,25 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+            description = "Compares two configuration files and shows a difference.",
+            version = "gendiff v.0.0.1")
+public class App implements Runnable{
+
+//    @Option(names = "--option", description = "Some option.")
+//    String option;
+
+    @Override
+    public void run() {
         System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
