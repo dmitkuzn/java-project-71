3981d667a528727d8e90ea5ee4a0b518b6ced7fe
Dmitrii Kuznetsov <dmitkuzn@mail.ru>
2024-10-07 23:01:48 +0300

    step5

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,6 +9,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
             description = "Compares two configuration files and shows a difference.",
@@ -31,7 +37,24 @@ public class App implements Runnable{
     }
 
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new App()).execute(args);
-        System.exit(exitCode);
+        App app = new App();
+        //int exitCode = new CommandLine(app.execute(args));
+        CommandLine.run(app, args);
+        Path absFilePath1 = Paths.get(app.filePath1).toAbsolutePath().normalize();
+        Path absFilePath2 = Paths.get(app.filePath2).toAbsolutePath().normalize();
+        try {
+            if (Files.exists(absFilePath1) && Files.exists(absFilePath2)) {
+                String fileContent1 = Files.readString(absFilePath1);
+                String fileContent2 = Files.readString(absFilePath2);
+                ObjectMapper objectMapper = new ObjectMapper();
+                Map<String, Object> resultMap1 = objectMapper.readValue(fileContent1, Map.class);
+                System.out.println(resultMap1);
+                Map<String, Object> resultMap2 = objectMapper.readValue(fileContent2, Map.class);
+                System.out.println(resultMap2);
+            }
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
