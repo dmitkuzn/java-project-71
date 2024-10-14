d4c357e44ffc91042a90a228e06531c8a5218110
Dmitrii Kuznetsov <dmitkuzn@mail.ru>
2024-10-14 22:36:54 +0300

    123

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,8 +1,5 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -27,50 +24,38 @@ public class Differ {
         Path absFilePath1 = Paths.get(filePath1).toAbsolutePath().normalize();
         Path absFilePath2 = Paths.get(filePath2).toAbsolutePath().normalize();
 
-        List<DiffNode> diffNodes = new ArrayList<>();
-        if (Files.exists(absFilePath1) && Files.exists(absFilePath2)) {
-            String fileContent1 = Files.readString(absFilePath1);
-            String fileContent2 = Files.readString(absFilePath2);
-
-            ObjectMapper objectMapper;
-            if (absFilePath1.toString().endsWith(".json")) {
-                objectMapper = new ObjectMapper();
-            } else if (absFilePath1.toString().endsWith(".yml") || absFilePath1.toString().endsWith(".yaml")) {
-                objectMapper = new ObjectMapper(new YAMLFactory());
-            } else {
-                System.out.println("Unknown file extension!");
-                return null;
-            }
-            Parser parser = new Parser(objectMapper);
-
-            Map<String, Object> map1 = parser.parse(fileContent1);
-            Map<String, Object> map2 = parser.parse(fileContent2);
-
-            Set<String> allKeys = new TreeSet<>();
-            allKeys.addAll(map1.keySet());
-            allKeys.addAll(map2.keySet());
-
-            for (String key : allKeys) {
-                boolean inMap1 = map1.containsKey(key);
-                boolean inMap2 = map2.containsKey(key);
-                if (inMap1 && !inMap2) {
-                    diffNodes.add(new DiffNode(key, DiffNode.Status.REMOVED, map1.get(key), null));
-                } else if (!inMap1 && inMap2) {
-                    diffNodes.add(new DiffNode(key, DiffNode.Status.ADDED, null, map2.get(key)));
-                } else {
-                    Object val1 = map1.get(key);
-                    Object val2 = map2.get(key);
-                    if (!Objects.equals(val1, val2)) {
-                        diffNodes.add(new DiffNode(key, DiffNode.Status.UPDATED, val1, val2));
-                    } else {
-                        diffNodes.add(new DiffNode(key, DiffNode.Status.UNCHANGED, val1, val2));
-                    }
-                }
-            }
-        } else {
+        if (!Files.exists(absFilePath1) || !Files.exists(absFilePath2)) {
             System.out.println("One or more files not found!");
             return null;
         }
+
+        List<DiffNode> diffNodes = new ArrayList<>();
+
+        Map<String, Object> map1 = Parser.parse(absFilePath1);
+        Map<String, Object> map2 = Parser.parse(absFilePath2);
+
+        Set<String> allKeys = new TreeSet<>();
+        allKeys.addAll(map1.keySet());
+        allKeys.addAll(map2.keySet());
+
+        for (String key : allKeys) {
+            boolean inMap1 = map1.containsKey(key);
+            boolean inMap2 = map2.containsKey(key);
+            if (inMap1 && !inMap2) {
+                diffNodes.add(new DiffNode(key, DiffNode.Status.REMOVED, map1.get(key), null));
+            } else if (!inMap1 && inMap2) {
+                diffNodes.add(new DiffNode(key, DiffNode.Status.ADDED, null, map2.get(key)));
+            } else {
+                Object val1 = map1.get(key);
+                Object val2 = map2.get(key);
+                if (!Objects.equals(val1, val2)) {
+                    diffNodes.add(new DiffNode(key, DiffNode.Status.UPDATED, val1, val2));
+                } else {
+                    diffNodes.add(new DiffNode(key, DiffNode.Status.UNCHANGED, val1, val2));
+                }
+            }
+        }
+
         Formatter formatter = FormatterFactory.getFormatter(formatName);
         return formatter.format(diffNodes);
     }
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -2,17 +2,25 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
+import java.nio.file.Path;
 import java.util.Map;
+import java.nio.file.Files;
 
 public final class Parser {
-    private ObjectMapper objectMapper;
 
-    public Parser(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
-
-    public Map<String, Object> parse(String fileContent) throws Exception {
+    public static Map<String, Object> parse(Path absFilePath) throws Exception {
+        String fileContent = Files.readString(absFilePath);
+        ObjectMapper objectMapper;
+        if (absFilePath.toString().endsWith(".json")) {
+            objectMapper = new ObjectMapper();
+        } else if (absFilePath.toString().endsWith(".yml") || absFilePath.toString().endsWith(".yaml")) {
+            objectMapper = new ObjectMapper(new YAMLFactory());
+        } else {
+            System.out.println("Unknown file extension!");
+            return null;
+        }
         return objectMapper.readValue(fileContent, new TypeReference<Map<String, Object>>() { });
     }
 }
